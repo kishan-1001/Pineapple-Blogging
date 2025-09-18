59d2cf3104345fba4ef72cc910e3c8f53a071712
kishan-1001 <roykishan532@gmail.com>
2025-09-18 12:26:11 +0530

    added animation to signin and singup

--- blogging website - frontend/src/pages/userAuthForm.page.jsx
+++ blogging website - frontend/src/pages/userAuthForm.page.jsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
 import InputBox from "../components/input.component";
 import googleIcon from "../imgs/google.png";
+import AnimationWrapper from "../common/page-animation";
 const UserAuthForm = ({type}) => {
   return (
+    <AnimationWrapper keyValue={type}>
     <section className="h-cover flex items-center justify-center">
         <form className="w-[80%] max-w-[400px] ">
             <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
@@ -51,6 +53,7 @@ const UserAuthForm = ({type}) => {
         </form>
 
     </section>
+    </AnimationWrapper>
   )
 }
 
